14c504509146173c3d7ae3fccfa2dffcd8a25011
jyoshnavi07 <jyoshnavi906@gmail.com>
2025-07-27 17:51:13 +0530

    Fix: Correct Profile component import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Home from './pages/users/pages/Home'
-import Profile from './pages/users/pages/profile'
+// import Profile from './pages/users/pages/profile'
 import Blog from './pages/users/pages/Blog'
 import Aboutus from './pages/users/pages/Aboutus'
 import Contact from './pages/users/pages/Contact'
 import Shop from './pages/users/pages/Shop'
 import { Route,Routes } from 'react-router-dom'
+import Profile from './pages/users/pages/Profile'
 
 
 const App = () => {
